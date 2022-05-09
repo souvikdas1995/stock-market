d5bf93b2f13b801c3274fa969f181f1676a94ffa
Souvik Das <svkds1995@gmail.com>
2022-05-09 16:59:34 +0530

    springboot application created

---
+++ src/test/java/com/cognizant/fse/stockmarket/StockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.fse.stockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
