420b608a4ef2fe82396a4e374fec90eae65bc88b
Souvik Das <svkds1995@gmail.com>
2022-06-22 22:01:41 +0530

    service registry configured

--- commandservice/src/main/java/command/service/CommandServiceApplication.java
+++ commandservice/src/main/java/command/service/CommandServiceApplication.java
@@ -4,6 +4,7 @@ import command.service.repository.CustomRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EntityScan("command.service.bean")
 @EnableJpaRepositories(repositoryBaseClass = CustomRepositoryImpl.class)
 public class CommandServiceApplication
--- queryservice/src/main/java/query/service/QueryServiceApplication.java
+++ queryservice/src/main/java/query/service/QueryServiceApplication.java
@@ -3,12 +3,14 @@ package query.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EntityScan("query.service.bean")
 public class QueryServiceApplication {
     public static void main(String[] args) {
